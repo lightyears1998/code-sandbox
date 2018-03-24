b5bc1116929a01213fd408a3d02df1c13fe39df2
lightyears <lightyears1998@hotmail.com>
2018-03-24 23:00:34 +0800

    at base

--- today/hello.cpp
+++ today/hello.cpp
@@ -1,47 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-using LL = long long;
- 
-const int maxN = 1000100;
 
-bool digit[maxN][10]; 
-LL nonrep[maxN];
- 
-bool check(LL num)
-{   
-    while (num) {
-        int cur = num%10;
-        if (digit[cur]) return false;
-        digit[cur] = true;
-        num /= 10;
-    }
-    return true;
-}
- 
-void build()
-{   
-    LL base = 1, gap = 0, tail = 0;
-    while (tail<maxN) {
-        for (int i=1; i<10 && tail<maxN; ++i)
-        {
-            for (int j=0; j<=gap && tail<maxN; ++j) 
-            {
-                if (check(i, nonrep[j]))
-                    nonrep[++tail] = i*base+nonrep[j];
-            }
-        } 
-        base *= 10, gap = tail;
-    }
-}
- 
+const int maxN = 50050;
+const int INF = 0x3F3F3F3F;
+
+int dp[]
+
 int main()
-{   
-    build();
-     
-    int T; scanf("%d", &T);
-    while (T--)
-    {
-        int n; scanf("%d", &n);
-        printf("%lld\n", nonrep[n]);
-    }
+{
+	
 }
--- today/temp.cpp
+++ today/temp.cpp
@@ -1,13 +1,21 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-long long num[200050];
+string des;
 
 int main()
 {
-	num[1] = 1, num[2] = 2;
-	for (int i=3; i<=100000; ++i) {
-		num[i] = num[i-1] + num[i-2];
-	}
-	cout << num[100000] << endl;
-} 
+    regex reg1(".*([aeiou]{3}|[^aeiou]{3}).*");
+    regex reg2(".*[aeiou]+.*");
+    regex reg3(".*([^eo])\\1.*");
+    while (cin >> des) {
+        if (des == "end")
+            break;
+        if (!regex_match(des, reg1) && regex_match(des, reg2) && !regex_match(des, reg3))
+            cout << "<" + des << "> is acceptable." << endl;
+        else
+            cout << "<" + des << "> is not acceptable." << endl;
+    }
+    return 0;
+}
