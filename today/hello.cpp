f4a37d13a43f5414c1f536952210e57afc4e2b17
lightyears <lightyears1998@hotmail.com>
2018-04-12 22:45:09 +0800

    at base

--- today/hello.cpp
+++ today/hello.cpp
@@ -1,44 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-const int maxN = 300300;
-
-string str[maxN];
-
-bool cmp(string &L, string &R, int len)
+void dijkstra()
 {
-    if (len == 0) return true;
-    for (int i=0; i<len; ++i) {
-        if (L[L.length()-len+i] != R[i])
-            return false;
-    }
-    return true;
-}
-
-
-int query(int l, int r)
-{
-    string &L = str[l], &R = str[r];
-    int len = min(L.length(), R.length());
-    while (!cmp(L, R, len)) --len;
-    return len;
-}
-
-int main()
-{
-	#ifdef LOCAL
-	freopen("in.txt", "r", stdin);
-	#endif 
+	memset(dis, 0x3F, sizeof(dis));
+	memset(vis, false, sizeof(vis));
+	dis[s] = 0;
 	
-    int n; cin >> n;
-    for (int i=0; i<n; ++i) {
-        cin >> str[i];
-    }
-    
-    int q; cin >> q;
-    while (q--)
-    {
-        int l, r; cin >> l >> r;
-        cout << query(l-1, r-1) << endl;
-    }
+	for (int i = 0; i < n && !vis[t]; ++i)
+	{
+		int marker = s, amin = INF;
+		for (int j = 0; j < n; ++j) if (!vis[j])
+		{
+			if (dis[j] < amin) {
+				marker = j;
+				amin = dis[j];
+			}
+		}
+		vis[marker] = true;
+		for (int j = 0; j < n; ++j) if (!vis[j])
+			dis[j] = min(dis[j], dis[marker] + path[marker][j]);
+	}
 }
