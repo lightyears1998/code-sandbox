31eee50d87ed0cd437538e88a6889dbb60ff2298
lightyears1998 <lightyears1998@hotmail.com>
2018-04-17 00:12:13 +0800

    at dorm

---
+++ today/csacnf.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	freopen("data.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	
+	long long cur;
+	while (scanf("%lld", &cur) == 1) {
+		printf("")
+	}
+	printf("%f\n", clock()/(double)CLOCKS_PER_SEC);
+}
