01932d544d01e69bad3a9b9d756018c57ea9efad
lightyears <lightyears1998@hotmail.com>
2018-04-18 11:46:45 +0800

    at base

--- homework/current.cpp
+++ homework/current.cpp
@@ -1,18 +1,63 @@
 #include <iostream>
 using namespace std;
 
-// 轮胎
-class Tire {
+class Vehicle
+{
+public:
+    Vehicle(int tire_number, int vehicle_weight)
+    {
+        tire_number_ = tire_number, vehicle_weight_ = vehicle_weight; 
+    }
+    virtual ~Vehicle() {}
+    virtual void speak() {
+        cout << "This vehicle has " << tire_number_ << " tires, it weights " << vehicle_weight_ << "kg.\n"; 
+    }
 protected:
-	int weight;	
+    int tire_number_;
+    int vehicle_weight_;
 };
 
-// 底盘
-class Chassis : private Tire {
+class Car : Vehicle
+{
+public:
+    Car(int tire_number, int vehicle_weight, int people_number) 
+        : Vehicle(tire_number, vehicle_weight) {
+        people_number_ = people_number; 
+    }
+    virtual ~Car() {}
+    virtual void speak() {
+        cout << "This car has " << tire_number_ << " tires, it weights " << vehicle_weight_ << "kg. ";
+        cout << "There " << (people_number_>1 ? "are " : "is ") << people_number_ << " people on board.\n";
+    }
+private:
+    int people_number_;
+};
 
-} 
+class Truck : Vehicle
+{
+public:
+    Truck(int tire_number, int vehicle_weight, int people_number, int load_weight) 
+        : Vehicle(tire_number, vehicle_weight) {
+        people_number_ = people_number, load_weight_ = load_weight; 
+    }
+    virtual ~Truck() {}
+    virtual void speak() {
+        cout << "This truck has " << tire_number_ << " tires, it weights " << vehicle_weight_ << "kg. ";
+        cout << "There " << (people_number_>1 ? "are " : "is ") << people_number_ << " people on board, ";
+        cout << "and it carrys " << load_weight_ << "kg load.\n";
+    }
+private:
+    int people_number_;
+    int load_weight_;
+};
 
 int main()
 {
-	
+    Vehicle vehicle(4, 10);
+    Car car(4, 10, 2);
+    Truck truck(12, 20, 1, 50);
+    
+    vehicle.speak();
+    car.speak();
+    truck.speak();
 }
--- today/hello.cpp
+++ today/hello.cpp
@@ -1,29 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-class Base {
-public:
-	int b;
-	
-	virtual void say() {
-		cout << "This is base." << endl;
-	}
-};
-
-class Child : public Base {
-public:
-	int c;
-	
-	void say() {
-		cout << "This is child." << endl;
-	}
-}; 
-
+ 
 int main()
 {
-	Child child;
-	Base * pbase = &child;
-	child.say();
-	pbase->say();
-	Child *pchild = static_cast<Child*>(pbase);
-} 
+	
+}
