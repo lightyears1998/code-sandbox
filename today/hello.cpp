7f2984ab1e29a5a7cd045dabc39d528a1fd5e0a1
lightyears <lightyears1998@hotmail.com>
2018-04-18 14:32:54 +0800

    at base

--- today/hello.cpp
+++ today/hello.cpp
@@ -1,7 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+class Apple
+{
+public:
+	Apple(int a, int b) {
+	}
+private:
+	int a_, b_;
+};
  
+ 
+class Bepple
+{
+public:
+	Bepple() {
+		
+	}
+private:
+	int x = 2, y = 1, z = y;
+};
+
 int main()
 {
-	
+	Bepple b;
 }
