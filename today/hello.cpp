a6b134153300f7dc7873a4e7eb7d45a58c6ed68f
lightyears <lightyears1998@hotmail.com>
2018-04-30 22:39:11 +0800

    at base

--- today/hello.cpp
+++ today/hello.cpp
@@ -1,7 +1,19 @@
-// 0: Sunday
-int whatday(int year, int month, int day)
+#include <bits/stdc++.h> 
+using namespace std;
+
+// 上次那个EOF的问题，我试了一下，
+// 感觉更好的策略是在输入流尝试读取数据之后检查输入流的错误位
+// 也可以直接使用 while (file >> number)，读取完所有有效数据之后循环就会终止 
+
+int main()
 {
-	int y=year%100, c=year/100, d=day;
-	int m=month+(month<=2?12:0);
-	return (y+y/4+c/4-2*c+(26*(m+1)/10)+d-1+7)%7;
+	fstream file("in.txt");
+	
+	int n;
+	while (!file.eof()) {
+		file >> n;
+		if (!file.eof()) 
+			cout << n << ' ';
+	} 
+	cout << endl;
 }
