6dba0b9c1194249c012043bf04a503ad11d30ff4
lightyears <lightyears1998@hotmail.com>
2018-05-01 12:31:37 +0800

    at base

--- today/BigInt.cpp
+++ today/BigInt.cpp
@@ -1,44 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct BigInt
+// 不能出现负数
+// 构造函数中memset是必须的，因为需要排除前导零 
+struct bint
 {
-	int v[1010], d; // 各位数值和长度 
+	int v[1010];  // 低位在前存放各个位置上的数据
+	int d;  // v中最后一位有效数字的下标
 	
-	BigInt() {
+	bint() {
 		memset(v, 0, sizeof(v));
 		d = 0;
 	}
 	
-	BigInt operator +(const BigInt b) const {
-		BigInt rslt;
-		int ent = 0;
-		for (int i=0; i<=d||i<=b.d||ent; ++i) {
-			int l = i<=d ? v[i] : 0;
-			int r = i<=b.d ? b.v[i] : 0;
-			int sum = l+r+ent;
-			ent = sum/10;
-			sum = sum%10;
-			rslt.v[d=i]=sum;
+	bint(const char *str, int len) {
+		memset(v, 0, sizeof(v)); d = len-1;
+		for (int i=0; i<len; ++i) {
+			v[i] = str[len-1-i]-'0';
 		}
-		return rslt;
+		while (d>0 && v[d]==0) --d;
 	}
 	
-	BigInt operator -(const BigInt b) const {
-		BigInt rlst;
-		int ent = 0; 
-	} 
+	bint(long long num) {
+		memset(v, 0, sizeof(v)); d = 0;
+		while (num) {
+			v[d++] = num%10;
+			num /= 10;
+		}
+		while (d>0 && v[d]==0) --d;
+	}
 	
 	void print() {
 		for (int i=d; i>=0; --i) {
 			printf("%d", v[i]);
 		}
+		putchar('\n');
 	}
+}; 
+
+bint operator +(const bint &a, const bint &b) {
+	bint rslt;
+	for (int i=0, ent=0; i<=a.d||i<=b.d||ent; ++i) {
+		int l = i<=a.d ? a.v[i] : 0;
+		int r = i<=b.d ? b.v[i] : 0;
+		int sum = l+r+ent;
+		ent = sum/10;
+		sum = sum%10;
+		rslt.v[rslt.d=i]=sum;
+	}
+	while (rslt.v[rslt.d]==0 && rslt.d!=0) --rslt.d;
+	return rslt;
 }
-using bint = BigInt;
+	
+bint operator -(const bint &a, const bint &b) {
+	bint rslt;
+	return rslt;
+}
 
 int main()
 {
-	bint a = 12;
-	cout << a << endl;
+	
 }
