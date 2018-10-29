c18aa4c1f64bde95963369820f85b7e11b82fd4d
lightyears <lightyears1998@hotmail.com>
2018-10-29 15:12:55 +0800

    “周五能用的代码周一不能用了”

--- snippet/Logkit/Logfilter/resource.h
+++ snippet/Logkit/Logfilter/resource.h
@@ -3,8 +3,8 @@
 // Used by Logfilter.rc
 //
 #define IDD_DESIGN                      101
-#define IDC_LIST1                       1002
 #define IDC_LIST2                       1003
+#define IDC_BUTTON1                     1004
 
 // Next default values for new objects
 // 
@@ -12,7 +12,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
