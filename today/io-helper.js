e1ba95ae4ada668181d2337caea30d6f3c230860
lightyears <lightyears1998@hotmail.com>
2018-12-17 02:09:44 +0800

    完成输入输出助手

---
+++ today/io-helper.js
@@ -0,0 +1,50 @@
+'use strict';
+
+function main() {
+    let n = nextInt(), ans = 0;
+    while (n--) {
+        let str = nextString();
+        str[1] === '+' ? ++ans : --ans;
+    }
+    console.log(ans);
+}
+
+// ------ IO Helper ------
+// nextInt(), nextFloat(), nextChar(), nextString()
+// isWhitespace(charater :string), clearWhitespaces()
+
+var input = "";
+var cursor = 0;
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(data) { input += data; });
+process.stdin.on('end', function() { main(); });
+
+var isWhitespace = (character) => (' \t\n\r\v'.indexOf(character) > -1);
+var clearWhitespaces = () => {
+    while (cursor < input.length && isWhitespace(input[cursor])) ++cursor; 
+};
+
+function nextInt() {
+    return parseInt(nextString());
+}
+
+function nextFloat() {
+    return parseFloat(nextString());
+}
+
+function nextChar() {
+    clearWhitespaces();
+    if (cursor < input.length)
+        return input[cursor++];
+    return '';
+}
+
+function nextString() {
+    let str = "";
+    clearWhitespaces();
+    while (cursor < input.length && !isWhitespace(input[cursor])) {
+        str += input[cursor++];
+    }
+    return str;
+}
