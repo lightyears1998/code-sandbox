ac00ce0a2458e299cf22173695dd260186bbe9b9
lightyears <lightyears1998@hotmail.com>
2019-10-11 01:07:27 +0800

    更新脚本

---
+++ .utils/remove-temp-and-binary-files.bat
@@ -0,0 +1,4 @@
+@echo on
+REM This scripts delete all temporary files and binary files from this repo.
+del /S "..\*.class"
+del /S "..\*.exe"
--- .utils/remove-temp-and-binary.bat
+++
@@ -1,3 +0,0 @@
-@echo on
-del /S "..\*.class"
-del /S "..\*.exe"
